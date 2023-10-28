de7317728c65d3d0d9d46342dce15caea2007958
Cyndi Cavanaugh <cyndicavanaugh5@gmail.com>
2023-10-28 12:34:59 -0500

    threed coming soon

--- displayimage.py
+++ displayimage.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import argparse
 from pathlib import Path
 from mayavi import mlab
 import numpy as np
@@ -9,13 +10,37 @@ def main():
     Displays a 3D point cloud
     Version 1.1 - Cyndi Cavanaugh
     '''
+    parser = argparse.ArgumentParser(description='Point cloud.')
+    parser.add_argument(
+        "--threed",
+        required=True,
+        default=None,
+        type=str,
+        help="path to file with 3d info")
+    parser.add_argument(
+        "--image",
+        required=True,
+        default=None,
+        type=str,
+        help="path to image")
+    args = parser.parse_args()
+    image_path = Path(args.image)
+    threed_path = Path(args.threed)
+    print(f'image_path={image_path} threed_path={threed_path}')
+    image = Image.open(image_path)
+    threed = Image.open(threed_path)
+    image.show()
+    threed.show()
+    threed_converted = threed.convert('L')
+    threed_data = np.asarray(threed_converted.getdata()).reshape(threed_converted.size)
+
     s = np.random.rand(20, 20, 20)
     volume = mlab.pipeline.volume(mlab.pipeline.scalar_field(s), vmin=0, vmax=0.8)
 
     mlab.draw()
     mlab.savefig('output.png')
-    img = Image.open('output.png')
-    img.show()
+    point = Image.open('output.png')
+    point.show()
 
 
 if __name__ == "__main__":
